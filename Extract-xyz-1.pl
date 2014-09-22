112c064ed725f314d8c0bf9ae0eb9f7c7aedaca3
Randal McGuckin <randal.mcguckin@gmail.com>
2014-09-22 13:44:05 +0100

    test

--- Extract-xyz-1.pl
+++ Extract-xyz-1.pl
@@ -1,17 +1,18 @@
 #!/usr/local/bin/perl -w
 # 314969.6411, 235527.2317, 0.0000
 # 315575.1240, 234870.3966, 0.0000
+# adding to make a test
 {
 	# to list all the lines between the above two points
 	# typical line:
 	# 308324.77735138 231273.05912018 64.12000275 255 255 255 0
-	
+
 	my $line;
 	my $x;
 	my $y;
 	my $z;
 	# my $icou;
-	
+
 	# points stored as (min, max)
 	my @pt=(314969.6411, 235527.2317, 0.0000);
 	my @pb=(315575.1240, 234870.3966, 0.0000);
@@ -38,14 +39,14 @@
 		$y[1]=$pt[1];
 	}
 	# print "x:$x[0]\t$x[1]\ny:$y[0]\t$y[1]\n";
-	
+
 	# open input and output files
 	my $file=q/C:\Data\Projects\xyz\Dublin-all.xyz/;
 	my $filout=q/>C:\Data\Projects\xyz\MMJ.xyz/;
-	
+
 	open (XYZ,$file) || die "Unable to find $file";
 	open (OUT,$filout) || die "Unable to find $filout";
-	
+
 	# $icou=0;
 	while(($line=<XYZ>))
 	{
